eb7dcb681a52c7f7c2ff96a0bfc6dd538442c7d3
Jieun Oh <0909oje@naver.com>
2022-11-28 20:56:46 +0900

    :art: App.js -> Graph.js 로 테스트 그래프 옮김

---
+++ app/src/page/Graph.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+import KOSPI from '../assets/KOSPI.json';
+
+const Graph = () => {
+  return (
+    <>
+      <Line options={options} data={data} />
+    </>
+  );
+};
+export default Graph;
+
+const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
+    },
+    title: {
+      display: true,
+      text: '테스트 개발 v1',
+    },
+  },
+};
+
+const labels = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+
+const data = {
+  labels: labels,
+  datasets: [
+    {
+      label: '코스피 지수',
+      data: KOSPI.map((item) => item.data),
+      borderColor: 'rgb(255, 99, 132)',
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    },
+    {
+      label: '내 포트폴리오',
+      // data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+      data: KOSPI.map((item) => item.data + 50),
+      borderColor: 'rgb(53, 162, 235)',
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    },
+  ],
+};
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
